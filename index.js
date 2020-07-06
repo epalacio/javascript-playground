40de1f692c7eb0ace839e8525954e23a69ccb0dd
Esteban Palacio <estipalacio@gmail.com>
2020-07-06 16:44:13 -0400

    Add main ex files

---
+++ index.js
@@ -0,0 +1,11 @@
+let a = '10';
+let b = '5';
+let c = '20';
+let d = '30';
+
+let total = (Number(a)) + (Number(b)) + (Number(c)) + (Number(d))
+let result = Math.ceil(total / 4);
+console.log(result);
+
+let randomDieRollOutcome = Math.floor(Math.random() * (7 - 1) + 1);
+console.log(randomDieRollOutcome);
